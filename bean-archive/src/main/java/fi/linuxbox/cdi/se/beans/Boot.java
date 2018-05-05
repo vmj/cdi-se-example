e74261b2df9f228e3b711ad55269406f8e40e6c3
Mikko Värri <vmj@linuxbox.fi>
2018-05-05 13:44:18 +0300

    OpenWebBeans behaves again (since 2.0.5).

--- bean-archive/src/main/java/fi/linuxbox/cdi/se/beans/Boot.java
+++ bean-archive/src/main/java/fi/linuxbox/cdi/se/beans/Boot.java
@@ -21,15 +21,12 @@ public class Boot {
         // SeContainerInitializer interface and loads that.
         // There can only be one.
         SeContainer seContainer = SeContainerInitializer.newInstance()
-                // OWB doesn't seem to find the beans.xml.
-                // By adding the fi.linuxbox.cdi.se.beans package to a
-                // synthetic bean archive, OWB finds all the beans, too.
-                .addPackages(BootEvent.class.getPackage())
-                // But then either discovery needs to be disabled,
-                // or the beans.xml needs to be removed.
-                // Otherwise WELD will find all the beans twice,
-                // leading to ambiguous injection points.
-                //.disableDiscovery()
+                // This JAR (bean-archive.jar) contains an empty beans.xml in the resources,
+                // so the CDI container will find beans from it.
+                // But if you have JARs that are not marked as bean archives,
+                // you could add some of their packages to the container and
+                // treat all those (CDI compatible POJOs) as beans:
+                //.addPackages(Foo.class.getPackage())
                 .initialize();
 
 
